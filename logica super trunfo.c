d66be266e3135ce60068381534423e8477266f6e
yuritorres23 <23yurigabriel@gmail.com>
2025-03-29 01:44:40 +0000

    switch

--- logica super trunfo.c
+++ logica super trunfo.c
@@ -45,54 +45,61 @@ int main(){
 
         valor2 = populacao2 + area2 + pib2 + pontosTuristicos2 - densidade_populacional2 + PIB_per_capta2;
         
-        printf("\n---Dados Carta 2---\n");
-        printf("O nome do estado é: %s\n", estado2);
-        printf("O código da carta é: %s\n", carta2);
-        printf("O nome da cidade é: %s\n", cidade2);
-        printf("A População de Manaus é: %d\n", populacao2);
-        printf("A Área (em km²) é: %.2f\n", area2);
-        printf("PIB (em bilhões): %.2f\n", pib2);
-        printf("Densidade populacional: %.2f\n", densidade_populacional2);
-        printf("Pib per capta: %.2f\n", PIB_per_capta2);
+        // menu do jogo
+        int opcao;
+        printf("\n---Escolha um numero para interagir---\n");
+        printf("1. População\n");
+        printf("2. Área\n");
+        printf("3. PIB\n");
+        printf("4. Pontos Turisticos\n");
+        printf("5. Densidade Populacional\n");
+        printf("6. Carta vencedora\n");
+        printf("Escolha uma opção!\n");
+        scanf("%d", opcao);
         
         printf("\n---Comparando os valores---\n");
 
-        if (populacao1 > populacao2) {
+        switch(opcao){
+        case 1:
+         if (populacao1 > populacao2) 
             printf("%s tem a maior população.\n", cidade1);
-        } else {
+         if else 
             printf("%s tem a maior população.\n", cidade2);
-        }
 
-        if (area1 > area2) {
+        
+
+         if (area1 > area2) 
             printf("%s tem a maior área(em km²).\n", cidade1);
-        } else {
+         if else 
             printf("%s tem a maior área(em km²).\n", cidade2);
-        }
+        
 
-        if (pib1 > pib2) {
+        if (pib1 > pib2) 
             printf("%s tem o maior PIB.\n", cidade1);
-        } else {
+        else 
             printf("%s tem o maior PIB.\n", cidade2);
-        }
+        
 
-        if (pontosTuristicos1 > pontosTuristicos2) {
+        if (pontosTuristicos1 > pontosTuristicos2) 
             printf("%s tem mais Pontos Turisticos.\n", cidade1);
-        } else {
+         if else 
             printf("%s tem mais Pontos Turisticos.\n", cidade2);
-        }
+        
 
-        if (densidade_populacional1 < densidade_populacional2) {
+        if (densidade_populacional1 < densidade_populacional2) 
             printf("%s tem a menor densidade populacional.\n", cidade1);
-        } else {
+         if else 
             printf("%s tem a menor densidade populacional.\n", cidade2);
-        }
+        
         
         //Determinando a carta vencedora
-        printf("\n   Descobrindo a carta vencedora   \n");
-        if (valor1 > valor2) {
+        printf("\n--- Descobrindo a carta vencedora ---\n");
+        if (valor1 > valor2) 
             printf("%s Venceu!\n", carta1);
-        } else {
+         else if (valor1 < valor2) 
             printf("%s Venceu!.\n", carta2);
+         else 
+            printf("Houve um empate!\n");
         }
 
 
